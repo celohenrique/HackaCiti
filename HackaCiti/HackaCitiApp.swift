6cf4f8ee62837c69b59b2101ae38d33a44dd25d2
Marcelo Henrique <celohenriquech7@gmail.com>
2023-06-18 13:24:41 -0300

    Initial Commit

---
+++ HackaCiti/HackaCitiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackaCitiApp.swift
+//  HackaCiti
+//
+//  Created by Marcelo Araujo on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HackaCitiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
